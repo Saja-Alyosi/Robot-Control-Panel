0f43bbc4bd678f3288b64cb6f7cb51ed39b4f1c9
Saja-Alyosi <saja.alyosi@gmail.com>
2026-08-16 01:11:45 +0300

    Add files via upload

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // بيانات الاتصال بقاعدة البيانات - غيّرها ببياناتك من InfinityFree
-$host = "sql108.infinityfree.com";      // اسم السيرفر (Hostname)
-$user = "if0_42423213";                // اسم المستخدم
-$pass = "TgzN3LZpt6Er24";           // كلمة المرور
-$dbname = "if0_42423213_db_robb";   // اسم قاعدة البيانات
+$host = "--";      // اسم السيرفر (Hostname)
+$user = "--";                // اسم المستخدم
+$pass = "--";           // كلمة المرور
+$dbname = "--";   // اسم قاعدة البيانات
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
